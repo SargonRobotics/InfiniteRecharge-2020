4d50e1a8e415d3cd62e9997a9cbd7a03431700fe
Nicholas Karr <nickkarr2005@gmail.com>
2020-01-18 11:43:03 -0600

    Add comments

--- InfiniteRecharge-2020/src/main/java/frc/robot/ColorThreshold.java
+++ InfiniteRecharge-2020/src/main/java/frc/robot/ColorThreshold.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
 public class ColorThreshold
@@ -18,8 +11,8 @@ public class ColorThreshold
         _greenMin = greenVal;
     }
 
-    public boolean inThreshhold(double redVal, double greenVal, double blueVal)
+    public boolean inThreshhold(double redVal, double greenVal, double blueVal) // Check if color is above a threshold in all three colors
     {
-        return (redVal >= _redMin && greenVal >= _greenMin && blueVal >= _blueMin);
+        return ((redVal >= _redMin) && (greenVal >= _greenMin) && (blueVal >= _blueMin));
     }
 }
